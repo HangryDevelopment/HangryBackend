4fbba01f494ce110ea86874a8734078f7a955aaf
Chase Forestello <101914667+Chase-Forestello@users.noreply.github.com>
2022-11-21 17:11:09 -0600

    first commit

---
+++ src/main/java/com/example/hangrybackend/HangrybackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.hangrybackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HangrybackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HangrybackendApplication.class, args);
+    }
+
+}
